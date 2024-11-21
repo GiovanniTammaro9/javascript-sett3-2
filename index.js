af801bf799ebe2147ccfd3a1c5e290b25ebb10d6
GiovanniTammaro9 <gio_play@live.it>
2024-11-21 15:57:45 +0100

    creato un oggetto javascript ed utilizzato il json

---
+++ index.js
@@ -0,0 +1,11 @@
+const listaNumeriInteri = {
+    titolo: "lista dei numeri interi",
+    elementi: [
+        "uno",
+        "due",
+        "tre"
+    ]
+};
+
+const listaNumeriInteriinJson = JSON.stringify(listaNumeriInteri);
+console.log(listaNumeriInteriinJson);
