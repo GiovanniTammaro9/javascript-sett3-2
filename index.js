01268e6352a1859ffac0620687d1a310d19db32b
GiovanniTammaro9 <gio_play@live.it>
2024-11-21 16:12:57 +0100

    manipolazione oggetti json

--- index.js
+++ index.js
@@ -14,3 +14,15 @@ console.log(listaNumeriInteriinJson);
 const oggettoJson = `{"nome": "mario"}`;
 const oggettoReale = JSON.parse(oggettoJson);
 console.log(oggettoReale.nome);
+
+//manipolazione di dati JSON
+
+let jsonString = `[{"nome": "Giovanni", "cognome": "Tammaro", "eta": "29"}, {"nome": "federica", "cognome": "longobardi", "eta": "27"}]`;
+//converto la stringa in un array di oggetti
+let persone = JSON.parse(jsonString);
+//aggiungo una nuova persona all'array
+persone.push({"nome": "ciro", "cognome": "de nicola", "eta": "42"});
+//converto l'array aggiornato in una stringa JSON
+let updatejsonString = JSON.stringify(persone);
+
+console.log(updatejsonString);
