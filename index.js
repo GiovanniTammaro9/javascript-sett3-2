b7da6b73ca09da28056a82aeeb770e11d8c7ac00
GiovanniTammaro9 <gio_play@live.it>
2024-11-21 16:00:55 +0100

    utilizzato il json.parse

--- index.js
+++ index.js
@@ -9,3 +9,8 @@ const listaNumeriInteri = {
 
 const listaNumeriInteriinJson = JSON.stringify(listaNumeriInteri);
 console.log(listaNumeriInteriinJson);
+
+//utilizzo il JSON.parse
+const oggettoJson = `{"nome": "mario"}`;
+const oggettoReale = JSON.parse(oggettoJson);
+console.log(oggettoReale.nome);
